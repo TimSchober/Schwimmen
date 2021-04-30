32fc844ed034834114c73d425d3300ac6d55fe2f
TimSchober <tims1014@gmail.com>
2021-04-30 02:59:38 +0200

    TUI added

--- src/main/scala/de/htwg/se/schwimmen/aUI/TUI.scala
+++ src/main/scala/de/htwg/se/schwimmen/aUI/TUI.scala
@@ -1,12 +1,5 @@
 package de.htwg.se.schwimmen.aUI
 
 class TUI {
-//  def processInputLine(input: String, board: Game, player: Player, dice: Dice): Game = {
-//    input match {
-//      case "1"|"2"|"3"|"4" => board.draw_pin(player, input.toInt - 1, dice.throwDice())
-//      case _   =>
-//        val new_input = scala.io.StdIn.readLine("No valid Pin, try again!\n")
-//        processInputLine(new_input, board, player, dice)
-//    }
-//  }
+
 }
