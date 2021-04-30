5c5f42f49c63986fb703eaa37f454d5abca055a4
TimSchober <tims1014@gmail.com>
2021-04-30 02:58:15 +0200

    Controller updated

--- src/main/scala/de/htwg/se/schwimmen/controller/Controller.scala
+++ src/main/scala/de/htwg/se/schwimmen/controller/Controller.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.schwimmen.controller
 
-class Controller {
+import de.htwg.se.schwimmen.model.CardStack
+import de.htwg.se.schwimmen.util.Observable
 
+class Controller(var stack: CardStack) extends Observable {
+  def createCardStack(): Unit = {
+    stack = new CardStack
+    notifyObservers
+  }
+  def stackPrint(): String = stack.toString
 }
