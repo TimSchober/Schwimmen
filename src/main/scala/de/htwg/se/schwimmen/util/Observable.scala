4a6e47ea049bdc97649fc0c3bb041d568315b890
TimSchober <tims1014@gmail.com>
2021-04-30 02:58:59 +0200

    Observable added

--- src/main/scala/de/htwg/se/schwimmen/util/Observable.scala
+++ src/main/scala/de/htwg/se/schwimmen/util/Observable.scala
@@ -1,5 +1,12 @@
 package de.htwg.se.schwimmen.util
 
-class Observable {
+trait Observer {
+  def update: Unit
+}
 
+class Observable {
+  var subscribers: Vector[Observer] = Vector()
+  def add(s: Observer) = subscribers=subscribers:+s
+  def remove(s: Observer) = subscribers = subscribers.filterNot(o=>o==s)
+  def notifyObservers = subscribers.foreach(o=>o.update)
 }
