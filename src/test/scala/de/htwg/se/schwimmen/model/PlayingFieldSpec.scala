8404ebb725f5d8865c189cb2b1d30a16e59e4104
TimSchober <tims1014@gmail.com>
2021-05-06 19:23:18 +0200

    100% coverage

--- src/test/scala/de/htwg/se/schwimmen/model/PlayingFieldSpec.scala
+++ src/test/scala/de/htwg/se/schwimmen/model/PlayingFieldSpec.scala
@@ -10,15 +10,18 @@ class PlayingFieldSpec extends AnyWordSpec with Matchers {
     val field = PlayingField(stack)
     val player = Player("player1", stack)
     "swap one card when call swapCard" in {
-      val fieldCopy = field.cardsOnField
-      val pl = field.swapCard(player.cardsOnHand, 2-1, 1 - 1)
-      field.cardsOnField.last should be(player.cardsOnHand(1))
-      pl.last should be(fieldCopy.head)
+      val fieldCardsList = field.cardsOnField
+      var plCards = field.swapCard(player.cardsOnHand, 0, 0)
+      plCards.head should be(fieldCardsList.head)
+      plCards = field.swapCard(player.cardsOnHand, 1, 1)
+      plCards(1) should be(fieldCardsList(1))
+      plCards = field.swapCard(player.cardsOnHand, 2, 2)
+      plCards.last should be(fieldCardsList.last)
     }
     "swap all cards when call swapAllCards" in {
       val fieldCopy = field.cardsOnField
-      val pl = field.swapAllCards(player.cardsOnHand)
-      pl should be(fieldCopy)
+      val playerCards = field.swapAllCards(player.cardsOnHand)
+      playerCards should be(fieldCopy)
     }
     "have three cards in cardsOnField" in {
       field.cardsOnField.length should be(3)
@@ -27,7 +30,15 @@ class PlayingFieldSpec extends AnyWordSpec with Matchers {
       field.processPlayerAmount(2) should be(true)
       field.processPlayerAmount(1) should be(false)
     }
-
+    "have a toString method" in {
+      val threeCards = stack.rndCardStack.take(3)
+      val field = PlayingField(stack)
+      val builder = new StringBuilder
+      builder.append("These are the cards on the field:    ")
+        .append(threeCards.head._1).append(" of ").append(threeCards.head._2).append("s    ")
+        .append(threeCards(1)._1).append(" of ").append(threeCards(1)._2).append("s    ")
+        .append(threeCards.last._1).append(" of ").append(threeCards.last._2).append("s    ")
+      field.toString should equal(builder.toString())
+    }
   }
-
 }
