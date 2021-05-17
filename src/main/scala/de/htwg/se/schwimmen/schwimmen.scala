99d703b44c5a15a5fd73d402471a10cb0177d4a8
TimSchober <tims1014@gmail.com>
2021-05-18 00:15:28 +0200

    only necessary code left

--- src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
+++ src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
@@ -7,40 +7,15 @@ import scala.io.StdIn.readLine
 
 object schwimmen {
 
-  val controller = new Controller(null, Nil, null, 0)
-  val tui = new TUI(controller)
+  val tui = new TUI(new Controller(null, Nil, null, null, 0))
 
   def main(args: Array[String]): Unit = {
-    println(tui.sayWelcome())
-    val playerAmount = readLine().toInt
-    if (tui.gamestart(playerAmount, List.tabulate(playerAmount){
-      n => readLine(s"Player ${n + 1}, type your name: ")
-    }) == -1) {
-      return
-    }
-    while(true) {
-      for(player <- tui.controller.players) {
-        if(player.hasKnocked) {
-          return
-        }
-        println()
-        println(tui.controller.field.toString)
-        println(player.toString)
-        println()
-        var processedInput = ""
-        do {
-          processedInput = tui.processInput(player, readLine(s"${player.name}, its your turn! " +
-            s"Do you want to change a card?(y/n) or all cards?(all) or knock?(k)"))
-          println(processedInput)
-        } while (processedInput.equals("illegal input, try again"))
-        if (!processedInput.equals("nextTurn")) {
-          tui.processInput2(player, readLine().toInt, readLine("which one of the field?(1/2/3)").toInt)
-        }
-        println()
-        println(tui.controller.field.toString)
-        println(player.toString)
-        println()
+    while (true) {
+      if (tui.input == "end") {
+        return
       }
+      tui.controller.notifyObservers()
+      tui.input = readLine()
     }
   }
 }
