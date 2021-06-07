f7a8d96c412d4178dfd611f3005624ec1abc10c2
TimSchober <tims1014@gmail.com>
2021-06-07 11:45:32 +0200

    UndoManager changed

--- src/main/scala/de/htwg/se/schwimmen/util/UndoManager.scala
+++ src/main/scala/de/htwg/se/schwimmen/util/UndoManager.scala
@@ -3,28 +3,27 @@ package de.htwg.se.schwimmen.util
 class UndoManager {
   private var undoStack: List[Command]= Nil
   private var redoStack: List[Command]= Nil
-  def doStep(command: Command) = {
+  def doStep(command: Command): Unit = {
     undoStack = command::undoStack
+    redoStack = command::redoStack
     command.doStep
   }
-  def undoStep  = {
+  def undoStep(): Unit  = {
     undoStack match {
       case  Nil =>
-      case head::stack => {
+      case head::stack =>
         head.undoStep
         undoStack=stack
         redoStack= head::redoStack
-      }
     }
   }
-  def redoStep = {
+  def redoStep(): Unit = {
     redoStack match {
       case Nil =>
-      case head::stack => {
+      case head::stack =>
         head.redoStep
         redoStack=stack
         undoStack=head::undoStack
-      }
     }
   }
 }
