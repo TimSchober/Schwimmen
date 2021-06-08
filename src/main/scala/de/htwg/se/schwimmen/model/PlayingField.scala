169633098ad32f09a1a0e7f0d57e4efa6ba1f4cd
TimSchober <tims1014@gmail.com>
2021-06-08 15:25:14 +0200

    swap method changed

--- src/main/scala/de/htwg/se/schwimmen/model/PlayingField.scala
+++ src/main/scala/de/htwg/se/schwimmen/model/PlayingField.scala
@@ -1,8 +1,29 @@
 package de.htwg.se.schwimmen.model
 
-case class PlayingField(stack: CardStack) extends AbstractPlayingFieldTemplate(stack: CardStack) {
+case class PlayingField(cardsOnField: List[(String, String)] = Nil) {
 
-  override def processPlayerAmount(amount: Int): Boolean = {
+  override def toString: String = {
+    val builder = new StringBuilder
+    builder.append("These are the cards on the field:    ")
+    for (x<-cardsOnField) {
+      builder.append(x._1).append(" of ").append(x._2).append("s    ")
+    }
+    builder.toString()
+  }
+
+  def setCardsOnField(threeCards: List[(String, String)]): PlayingField = {
+    copy(cardsOnField = threeCards)
+  }
+
+  def swapCard(player: Player, indexPlayer: Int, indexField: Int): List[(String, String)] = {
+    indexField match {
+      case 0 => List(player.cardsOnHand(indexPlayer), cardsOnField(1), cardsOnField.last)
+      case 1 => List(cardsOnField.head, player.cardsOnHand(indexPlayer), cardsOnField.last)
+      case 2 => List(cardsOnField.head, cardsOnField(1), player.cardsOnHand(indexPlayer))
+    }
+  }
+
+  def processPlayerAmount(amount: Int): Boolean = {
     amount match {
       case 2|3|4|5|6|7|8|9 => true
       case _ => false
--- src/test/scala/de/htwg/se/schwimmen/model/PlayingFieldSpec.scala
+++ src/test/scala/de/htwg/se/schwimmen/model/PlayingFieldSpec.scala
@@ -7,37 +7,33 @@ class PlayingFieldSpec extends AnyWordSpec with Matchers {
 
   "A PlayingField" should {
     val stack = CardStack()
-    val field = PlayingField(stack)
-    val player = Player("player1", stack)
-    "swap one card when call swapCard" in {
-      val fieldCardsList = field.cardsOnField
-      var plCards = field.swapCard(player.cardsOnHand, 0, 0)
-      plCards.head should be(fieldCardsList.head)
-      plCards = field.swapCard(player.cardsOnHand, 1, 1)
-      plCards(1) should be(fieldCardsList(1))
-      plCards = field.swapCard(player.cardsOnHand, 2, 2)
-      plCards.last should be(fieldCardsList.last)
+    var field = PlayingField()
+    var player = Player("Tim")
+    "set the given cards" in {
+      val fieldli = List(("7","hearts"),("8","hearts"),("9","hearts"))
+      field = field.setCardsOnField(fieldli)
+      field.cardsOnField should equal(fieldli)
     }
-    "swap all cards when call swapAllCards" in {
-      val fieldCopy = field.cardsOnField
-      val playerCards = field.swapAllCards(player.cardsOnHand)
-      playerCards should be(fieldCopy)
-    }
-    "have three cards in cardsOnField" in {
-      field.cardsOnField.length should be(3)
+    "give back a list of cards on the field after player has swapped" in {
+      val playerli = List(("10","spade"),("jack","spade"),("queen","spade"))
+      val fieldli = List(("7","hearts"),("8","hearts"),("9","hearts"))
+      player = player.setCardsOnHand(playerli)
+      field = field.setCardsOnField(fieldli)
+      field.swapCard(player, 0, 0) should equal(List(("10","spade"),("8","hearts"),("9","hearts")))
+      field.swapCard(player, 0, 1) should equal(List(("7","hearts"),("10","spade"),("9","hearts")))
+      field.swapCard(player, 0, 2) should equal(List(("7","hearts"),("8","hearts"),("10","spade")))
     }
     "have 2-9 player" in {
       field.processPlayerAmount(2) should be(true)
       field.processPlayerAmount(1) should be(false)
     }
     "have a toString method" in {
-      val threeCards = stack.rndCardStack.take(3)
-      val field = PlayingField(stack)
+      field = field.setCardsOnField(List(("7","hearts"),("8","hearts"),("9","hearts")))
       val builder = new StringBuilder
       builder.append("These are the cards on the field:    ")
-        .append(threeCards.head._1).append(" of ").append(threeCards.head._2).append("s    ")
-        .append(threeCards(1)._1).append(" of ").append(threeCards(1)._2).append("s    ")
-        .append(threeCards.last._1).append(" of ").append(threeCards.last._2).append("s    ")
+        .append("7").append(" of ").append("hearts").append("s    ")
+        .append("8").append(" of ").append("hearts").append("s    ")
+        .append("9").append(" of ").append("hearts").append("s    ")
       field.toString should equal(builder.toString())
     }
   }
