c8014405e8537f9db1bd504f5d89683f74fb81ef
TimSchober <tims1014@gmail.com>
2021-06-11 11:14:58 +0200

    changed

--- src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
+++ src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
@@ -1,16 +1,19 @@
 package de.htwg.se.schwimmen
 
 import de.htwg.se.schwimmen.aUI.{GUI, TUI}
-import de.htwg.se.schwimmen.controller.Controller
+import de.htwg.se.schwimmen.controller.{Controller, NewGame}
+import de.htwg.se.schwimmen.model.{CardStack, PlayingField}
 
 import scala.io.StdIn.readLine
 
 object schwimmen {
 
-  val controller = new Controller(null, Nil, null, 0)
+  val stack: CardStack = CardStack()
+  val field: PlayingField = PlayingField(stack.getThreeCards)
+  val controller = new Controller(stack, Nil, field, 0)
   val tui = new TUI(controller)
   val gui = new GUI(controller)
-  tui.controller.createNewGame()
+  controller.publish(new NewGame)
 
   def main(args: Array[String]): Unit = {
     do {
