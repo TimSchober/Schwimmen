b1d22c091bc5f2f80a3036a015ec466c4dcd8d55
TimSchober <tims1014@gmail.com>
2021-06-18 11:49:42 +0200

    Injection added

--- src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
+++ src/main/scala/de/htwg/se/schwimmen/schwimmen.scala
@@ -3,9 +3,6 @@ package de.htwg.se.schwimmen
 import com.google.inject.{Guice, Injector}
 import de.htwg.se.schwimmen.aUI.{GUI, TUI}
 import de.htwg.se.schwimmen.controller.controllerComponent._
-import de.htwg.se.schwimmen.controller.controllerComponent.controllerImpl.Controller
-import de.htwg.se.schwimmen.model.cardStackComponent.cardStackImpl.CardStack
-import de.htwg.se.schwimmen.model.fieldComponent.fieldImpl.PlayingField
 
 import scala.io.StdIn.readLine
 
@@ -13,7 +10,7 @@ object schwimmen {
 
   val injector: Injector = Guice.createInjector(new schwimmenModul)
   val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
-  //val controller = new Controller(CardStack(), Nil, PlayingField(), 0)
+  controller.createNewGame()
   val tui = new TUI(controller)
   val gui = new GUI(controller)
   controller.publish(new NewGame)
--- src/main/scala/de/htwg/se/schwimmen/schwimmenModul.scala
+++ src/main/scala/de/htwg/se/schwimmen/schwimmenModul.scala
@@ -1,8 +1,28 @@
 package de.htwg.se.schwimmen
 
 import com.google.inject.AbstractModule
+import com.google.inject.name.Names
+import de.htwg.se.schwimmen.controller.controllerComponent._
+import de.htwg.se.schwimmen.model.cardStackComponent._
+import de.htwg.se.schwimmen.model.cardStackComponent.cardStackImpl.CardStack
+import de.htwg.se.schwimmen.model.fieldComponent._
+import de.htwg.se.schwimmen.model.fieldComponent.fieldImpl.{Player, PlayingField}
 import net.codingwell.scalaguice.ScalaModule
 
 class schwimmenModul extends  AbstractModule with ScalaModule {
 
+  override def configure(): Unit = {
+    bindConstant().annotatedWith(Names.named("hasKnocked")).to(false)
+    bindConstant().annotatedWith(Names.named("plAm")).to(0)
+    bindConstant().annotatedWith(Names.named("cardCount")).to(0.0)
+    bindConstant().annotatedWith(Names.named("life")).to(3)
+
+    bind[ControllerInterface].to[controllerImpl.Controller]
+    bind[CardStackInterface].to[CardStack]
+    bind[PlayingFieldInterface].to[PlayingField]
+    bind[PlayerInterface].to[Player]
+
+    bind[List[PlayerInterface]].toInstance(List[Player]())
+    bind[List[(String, String)]].toInstance(List[(String, String)]())
+  }
 }
