5ef55e3c89de335f50b4b11531676c7e5ea99321
TimSchober <ti412sch@htwg-konstanz.de>
2023-04-25 21:09:40 +0200

    implement API with akka

---
+++ cards/src/main/scala/de/htwg/se/schwimmen/cardStackComponent/CardStackAPI.scala
@@ -0,0 +1,35 @@
+package de.htwg.se.schwimmen.cardStackComponent
+
+import akka.http.scaladsl.server.Directives.*
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.model.*
+import com.google.inject.{Guice, Injector}
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+import de.htwg.se.schwimmen.cardStackComponent.CardStackInterface
+import de.htwg.se.schwimmen.cardStackComponent.cardStackModul
+
+@main def CardStackAPI() = {
+
+  implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "CardStackAPI")
+
+  val injector: Injector = Guice.createInjector(new cardStackModul)
+  var stack = injector.getInstance(classOf[CardStackInterface])
+  stack = stack.setCardStack()
+
+  val route =
+    path("cardStack") {
+      get {
+        val threeCards: String = stack.getThreeCardsInJsonFormat
+        stack = stack.delThreeCards
+        complete(HttpEntity(ContentTypes.`application/json`, threeCards))
+      }
+    }
+
+  Http().newServerAt("127.0.0.1", 8080).bind(route)
+
+}
