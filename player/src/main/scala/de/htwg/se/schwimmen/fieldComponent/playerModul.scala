b85668dc8c094b9e8df76c4ffc0426e67e847901
TimSchober <ti412sch@htwg-konstanz.de>
2023-04-25 23:49:20 +0200

    add module for Player api

---
+++ player/src/main/scala/de/htwg/se/schwimmen/fieldComponent/playerModul.scala
@@ -0,0 +1,25 @@
+package de.htwg.se.schwimmen
+
+import com.google.inject.AbstractModule
+import com.google.inject.name.Names
+import de.htwg.se.schwimmen.fieldComponent.{PlayerInterface, PlayingFieldInterface}
+import de.htwg.se.schwimmen.fieldComponent.fieldImpl.{Player, PlayingField}
+import de.htwg.se.schwimmen.fieldComponent.*
+import net.codingwell.scalaguice.ScalaModule
+
+class playerModul extends AbstractModule with ScalaModule {
+
+  override def configure(): Unit = {
+    bindConstant().annotatedWith(Names.named("hasKnocked")).to(false)
+    bindConstant().annotatedWith(Names.named("plAm")).to(0)
+    bindConstant().annotatedWith(Names.named("cardCount")).to(0.0)
+    bindConstant().annotatedWith(Names.named("life")).to(3)
+    
+    bind[PlayingFieldInterface].to[PlayingField]
+    bind[PlayerInterface].to[Player]
+
+    bind[Option[String]].toInstance(None)
+    bind[List[PlayerInterface]].toInstance(List[Player]())
+    bind[List[(String, String)]].toInstance(List[(String, String)]())
+  }
+}
