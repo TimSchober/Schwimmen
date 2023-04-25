57a43f27d9ff577bc36698fdc366f3d12a0cdbcd
TimSchober <ti412sch@htwg-konstanz.de>
2023-04-25 20:14:39 +0200

    test card stack api

--- cards/src/main/scala/de/htwg/se/schwimmen/cardStackComponent/cardStackImpl/CardStackAPI.scala
+++ cards/src/main/scala/de/htwg/se/schwimmen/cardStackComponent/cardStackImpl/CardStackAPI.scala
@@ -1,39 +1,26 @@
 package de.htwg.se.schwimmen.cardStackComponent.cardStackImpl
-import de.htwg.se.schwimmen.cardStackComponent.cardStackImpl.CardStack
 
+import akka.http.scaladsl.server.Directives.{complete, concat, get, path}
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives.*
-import scala.io.StdIn
-import play.api.libs.json._
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCode}
+import akka.http.scaladsl.server.{ExceptionHandler, Route}
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.util.{Failure, Success}
+import scala.io.StdIn
 
-object CardStackAPI {
-  // needed to run the route
-  implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "my-system")
-  // needed for the future flatMap/onComplete in the end
-  val executionContext: ExecutionContextExecutor = system.executionContext
+@main def CardStackAPI() = {
 
-  given ExecutionContextExecutor = executionContext
+  implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "akka-http")
 
-  var cardStack: CardStack = CardStack()
-  cardStack = cardStack.setCardStack()
-  val json: JsValue = Json.toJson(cardStack.getThreeCards)
-
-  val route = concat(
-    path("CardStack" / "getThreeCards") {
-      get {
-        complete(HttpEntity(ContentTypes.`application/json`, json))
-      }
+  val route = get {
+    path("test") {
+      complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "test"))
     }
-  )
+  }
 
-  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
-
-  println(s"Server now online. Please navigate to http://localhost:8080/fileio\nPress RETURN to stop...")
+  Http().newServerAt("127.0.0.1", 8080).bind(route)
 
 }
