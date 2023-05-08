de2dab7216b3b89eb2c501168d42c2712bc6c202
TimSchober <ti412sch@htwg-konstanz.de>
2023-05-08 22:24:33 +0200

    add fieldDataToJsonObject in PlayingField for converting the PlayingField into a Json Format

--- player/src/main/scala/de/htwg/se/schwimmen/fieldComponent/fieldImpl/PlayingField.scala
+++ player/src/main/scala/de/htwg/se/schwimmen/fieldComponent/fieldImpl/PlayingField.scala
@@ -3,6 +3,7 @@ package de.htwg.se.schwimmen.fieldComponent.fieldImpl
 import com.google.inject.Inject
 import de.htwg.se.schwimmen.fieldComponent.{PlayerInterface, PlayingFieldInterface}
 import de.htwg.se.schwimmen.fieldComponent.*
+import play.api.libs.json.{JsObject, Json}
 
 case class PlayingField @Inject() (cardsOnField: List[(String, String)] = Nil) extends PlayingFieldInterface{
 
@@ -32,5 +33,24 @@ case class PlayingField @Inject() (cardsOnField: List[(String, String)] = Nil) e
       case 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => true
       case _ => false
     }
+  }
+
+  def fieldDataToJsonObject: JsObject = {
+    Json.obj(
+      "cardsOnField" -> Json.obj(
+        "firstCard" -> Json.obj(
+          "Value" -> cardsOnField.head._1,
+          "Color" -> cardsOnField.head._2
+        ),
+        "secondCard" -> Json.obj(
+          "Value" -> cardsOnField(1)._1,
+          "Color" -> cardsOnField(1)._2
+        ),
+        "thirdCard" -> Json.obj(
+          "Value" -> cardsOnField.last._1,
+          "Color" -> cardsOnField.last._2
+        )
+      )
+    )
   }
 }
