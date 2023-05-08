3f288e3c6342d42de496ba7dbc15a2e4e317b8f2
TimSchober <ti412sch@htwg-konstanz.de>
2023-05-08 22:21:48 +0200

    change gui to use the response for displaying the cards

--- src/main/scala/de/htwg/se/schwimmen/aUI/GUI.scala
+++ src/main/scala/de/htwg/se/schwimmen/aUI/GUI.scala
@@ -1,10 +1,11 @@
 package de.htwg.se.schwimmen.aUI
 
-import de.htwg.se.schwimmen.controller.controllerComponent._
+import de.htwg.se.schwimmen.controller.controllerComponent.*
+import play.api.libs.json.JsValue
 
 import java.io.File
 import javax.imageio.ImageIO
-import scala.swing._
+import scala.swing.*
 
 class GUI(controller: ControllerInterface) extends Frame {
 
@@ -13,12 +14,12 @@ class GUI(controller: ControllerInterface) extends Frame {
 
   def paintCards: Panel = new Panel {
     override def paint(g:Graphics2D): Unit = {
-      g.drawImage(ImageIO.read(new File(getPath(controller.field.cardsOnField.head))),200,50,null)
-      g.drawImage(ImageIO.read(new File(getPath(controller.field.cardsOnField(1)))),360,50,null)
-      g.drawImage(ImageIO.read(new File(getPath(controller.field.cardsOnField.last))),520,50,null)
-      g.drawImage(ImageIO.read(new File(getPath(controller.players.head.cardsOnHand.head))),200,300,null)
-      g.drawImage(ImageIO.read(new File(getPath(controller.players.head.cardsOnHand(1)))),360,300,null)
-      g.drawImage(ImageIO.read(new File(getPath(controller.players.head.cardsOnHand.last))),520,300,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.currentField \ "data" \ "cardsOnField" \ "firstCard").get))),200,50,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.currentField \ "data" \ "cardsOnField" \ "secondCard").get))),360,50,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.currentField \ "data" \ "cardsOnField" \ "thirdCard").get))),520,50,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.headPlayer \ "data" \ "cardsOnHand" \ "firstCard").get))),200,300,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.headPlayer \ "data" \ "cardsOnHand" \ "secondCard").get))),360,300,null)
+      g.drawImage(ImageIO.read(new File(getPath((controller.headPlayer \ "data" \ "cardsOnHand" \ "thirdCard").get))),520,300,null)
     }
   }
 
@@ -31,10 +32,7 @@ class GUI(controller: ControllerInterface) extends Frame {
     contents += Button("hand(1)") {handIndex = 1}
     contents += Button("hand(2)") {handIndex = 2}
     contents += Button("hand(3)") {handIndex = 3}
-    val playerName = controller.players.head.name match {
-      case Some(s) => s
-      case None => ""
-    }
+    val playerName: String = (controller.headPlayer \ "data" \ "name").get.toString.replaceAll("\"", "")
     contents += new Label(playerName)
     val changeOneCardButton: Button = Button("ChangeOneCard") {
       controller.swapCards(handIndex - 1, fieldIndex - 1)
@@ -80,11 +78,13 @@ class GUI(controller: ControllerInterface) extends Frame {
   preferredSize = new Dimension(1000,700)
   visible = true
 
-  def getPath(card: (String,String)): String = {
+  def getPath(card: JsValue): String = {
     "src/main/scala/de/htwg/se/schwimmen/aUI/textures/" + {
-      card._2 match {
+      val value = (card \ "Value").get.toString.replaceAll("\"", "")
+      val color = (card \ "Color").get.toString.replaceAll("\"", "")
+      color match {
         case "club" =>
-          card._1 match {
+          value match {
             case "7" => "7_of_clubs.png"
             case "8" => "8_of_clubs.png"
             case "9" => "9_of_clubs.png"
@@ -95,7 +95,7 @@ class GUI(controller: ControllerInterface) extends Frame {
             case "ace" => "ace_of_clubs.png"
           }
         case "diamond" =>
-          card._1 match {
+          value match {
             case "7" => "7_of_diamonds.png"
             case "8" => "8_of_diamonds.png"
             case "9" => "9_of_diamonds.png"
@@ -106,7 +106,7 @@ class GUI(controller: ControllerInterface) extends Frame {
             case "ace" => "ace_of_diamonds.png"
           }
         case "heart" =>
-          card._1 match {
+          value match {
             case "7" => "7_of_hearts.png"
             case "8" => "8_of_hearts.png"
             case "9" => "9_of_hearts.png"
@@ -117,7 +117,7 @@ class GUI(controller: ControllerInterface) extends Frame {
             case "ace" => "ace_of_hearts.png"
           }
         case "spade" =>
-          card._1 match {
+          value match {
             case "7" => "7_of_spades.png"
             case "8" => "8_of_spades.png"
             case "9" => "9_of_spades.png"
