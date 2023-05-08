66e76a12eb19bc7dfaa96534f423bad09097f4ff
TimSchober <ti412sch@htwg-konstanz.de>
2023-05-08 22:27:00 +0200

    delete everything from other microservices

--- src/main/scala/de/htwg/se/schwimmen/schwimmenModul.scala
+++ src/main/scala/de/htwg/se/schwimmen/schwimmenModul.scala
@@ -2,36 +2,16 @@ package de.htwg.se.schwimmen
 
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
-import de.htwg.se.schwimmen.cardStackComponent.CardStackInterface
-import de.htwg.se.schwimmen.cardStackComponent.cardStackImpl.CardStack
 import de.htwg.se.schwimmen.controller.controllerComponent.*
-import de.htwg.se.schwimmen.fieldComponent.{PlayerInterface, PlayingFieldInterface}
-import de.htwg.se.schwimmen.fieldComponent.fieldImpl.{Player, PlayingField}
-import de.htwg.se.schwimmen.fileIOComponent.fileIOJsonImpl.FileIO
-import de.htwg.se.schwimmen.fileIOComponent.{FileIOInterface, fileIOXmlImpl}
-import de.htwg.se.schwimmen.cardStackComponent.*
-import de.htwg.se.schwimmen.fieldComponent.*
-import de.htwg.se.schwimmen.fileIOComponent.*
 import net.codingwell.scalaguice.ScalaModule
 
 class schwimmenModul extends  AbstractModule with ScalaModule {
 
   override def configure(): Unit = {
-    bindConstant().annotatedWith(Names.named("hasKnocked")).to(false)
-    bindConstant().annotatedWith(Names.named("plAm")).to(0)
-    bindConstant().annotatedWith(Names.named("cardCount")).to(0.0)
-    bindConstant().annotatedWith(Names.named("life")).to(3)
 
     bind[ControllerInterface].to[controllerImpl.Controller]
-    bind[CardStackInterface].to[CardStack]
-    bind[PlayingFieldInterface].to[PlayingField]
-    bind[PlayerInterface].to[Player]
 
-    bind[Option[String]].toInstance(None)
-    bind[List[PlayerInterface]].toInstance(List[Player]())
     bind[List[(String, String)]].toInstance(List[(String, String)]())
 
-    bind[FileIOInterface].annotatedWithName("Xml").to[fileIOXmlImpl.FileIO]
-    bind[FileIOInterface].annotatedWithName("Json").to[FileIO]
   }
 }
