8069ceba7c922373229896f9319822dbee781291
TimSchober <ti412sch@htwg-konstanz.de>
2023-05-11 20:28:01 +0200

    change card stack api to object

--- cards/src/main/scala/de/htwg/se/schwimmen/cardStackComponent/CardStackAPI.scala
+++ cards/src/main/scala/de/htwg/se/schwimmen/cardStackComponent/CardStackAPI.scala
@@ -1,40 +1,67 @@
 package de.htwg.se.schwimmen.cardStackComponent
 
-import akka.http.scaladsl.server.Directives.*
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives.*
-import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+//import akka.http.scaladsl.server.Directives.*
+//import akka.actor.typed.ActorSystem
+//import akka.actor.typed.scaladsl.Behaviors
+//import akka.http.scaladsl.Http
+//import akka.http.scaladsl.server.Directives.*
+//import akka.http.scaladsl.model.*
 import com.google.inject.{Guice, Injector}
 
-import scala.concurrent.ExecutionContextExecutor
+// import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import scala.io.StdIn
 import de.htwg.se.schwimmen.cardStackComponent.CardStackInterface
 import de.htwg.se.schwimmen.cardStackComponent.cardStackModul
 import play.api.libs.json.{JsObject, Json}
 
-@main def CardStackAPI(): Unit = {
+object CardStackAPI {
+  // implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "CardStackAPI")
 
-  implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "CardStackAPI")
+  private var server: Option[Http.ServerBinding] = None
 
-  val injector: Injector = Guice.createInjector(new cardStackModul)
-  var stack = injector.getInstance(classOf[CardStackInterface])
-  stack = stack.setCardStack()
+  given system: ActorSystem = ActorSystem("DiceService")
 
-  val route =
-    path("cardStack" / "threeCards") {
-      get {
-        val threeCards: JsObject = stack.getThreeCardsInJsonFormat
-        stack = stack.delThreeCards
-        complete(HttpEntity(ContentTypes.`application/json`, Json.obj(
-          "success" -> true,
-          "data" -> threeCards
-        ).toString))
-      }
+  @main def main(): Unit = {
+
+    val injector: Injector = Guice.createInjector(new cardStackModul)
+    var stack = injector.getInstance(classOf[CardStackInterface])
+    stack = stack.setCardStack()
+
+    val route =
+      concat(
+        path("cardStack" / "threeCards") {
+          get {
+            val threeCards: JsObject = stack.getThreeCardsInJsonFormat
+            stack = stack.delThreeCards
+            complete(HttpEntity(ContentTypes.`application/json`, Json.obj(
+              "success" -> true,
+              "data" -> threeCards
+            ).toString))
+          }
+        },
+        path("test") {
+          get {
+            complete("OK")
+          }
+        }
+      )
+
+    val server = Some(Http().newServerAt("0.0.0.0", 8080).bind(route))
+    server.get.map { _ =>
+      println(s"Server now online. Please navigate to http://localhost:8080/cardStack\n")
+    } recover { case ex =>
+      println(s"Server could not start: ${ex.getMessage}")
     }
+    //  Http().newServerAt("0.0.0.0", 8080).bind(route)
+    //  println(s"Server now online. Please navigate to http://localhost:8080/cardStack\n")
 
-  Http().newServerAt("127.0.0.1", 8080).bind(route)
-  println(s"Server now online. Please navigate to http://localhost:8080/cardStack\n")
-
+  }
 }
